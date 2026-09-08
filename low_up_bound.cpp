3d0f7bf66145e10dd1a253999e60bff9336ab13b
ArjitSingh0907 <singh25arjit@gmail.com>
2026-09-08 20:54:15 +0530

    printing lower to upper bound within a range

---
+++ low_up_bound.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main() {
+    set<int> s = {5,10,15,20,25,30};
+
+    int low = 10, high = 25;
+    auto start = s.lower_bound(low);
+    auto end = s.upper_bound(high);
+
+    cout << "Elements between " << low << " and " << high << ": ";
+    for(auto it = start; it != end; ++it){
+        cout << *it << " ";
+    }
+    cout << endl;
+    return 0;
+}

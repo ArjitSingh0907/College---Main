5daf6c3efabb20df0ff226b7bc22f3963a5837e5
ArjitSingh0907 <singh25arjit@gmail.com>
2026-09-08 20:50:59 +0530

    search via key in map

---
+++ key_srch.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+int main() {
+    map<int,string> m = {{99,"Saman"}, {100,"Rhea"}, {101,"Arjit"}};
+
+    int key = 101;
+    auto it = m.find(key);
+
+    if(it != m.end())
+        cout << "Found -> " << it->first << " : " << it->second << endl;
+    else
+        cout << "Not found." << endl;
+}

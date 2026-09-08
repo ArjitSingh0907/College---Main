5523c86d85f54b51c12a530dcef3439af601cb92
ArjitSingh0907 <singh25arjit@gmail.com>
2026-09-08 20:51:41 +0530

    handling duplicates in multimap

---
+++ map_dup.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+int main() {
+    multimap<int,string> mm = {
+        {102,"Ayesha"}, {102,"Sanya"}, {102,"Laptop"}
+    };
+
+    int key = 102;
+    auto range = mm.equal_range(key);
+
+    cout << "Entries for roll " << key << ":" << endl;
+    for(auto it = range.first; it != range.second; ++it){
+        cout << it->first << " : " << it->second << endl;
+    }
+    return 0;
+}

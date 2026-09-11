1fd38ec729628ac69ec423dc8ad47d3b082e5813
ArjitSingh0907 <singh25arjit@gmail.com>
2026-09-11 22:33:05 +0530

    length of a string using for loop

---
+++ str_len5.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char str[100];
+    int length = 0;
+
+    cout << "Enter a string: ";
+    cin >> str;
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        length++;
+    }
+
+    cout << "Length = " << length << endl;
+
+    return 0;
+}

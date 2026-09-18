bba4c65c57b8a60485305fa5481831a6b2f49ef8
ArjitSingh0907 <singh25arjit@gmail.com>
2026-09-18 20:58:12 +0530

    insert elm at user-specified pos

---
+++ arr_ins3.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[20] = {10, 20, 30, 40, 50};
+    int size = 5;
+    int element, pos;
+
+    cout << "Enter element: ";
+    cin >> element;
+
+    cout << "Enter position: ";
+    cin >> pos;
+
+    for(int i = size; i > pos; i--) {
+        arr[i] = arr[i - 1];
+    }
+
+    arr[pos] = element;
+    size++;
+
+    cout << "Updated array: ";
+
+    for(int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}

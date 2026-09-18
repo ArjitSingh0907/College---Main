2964d8d1423bd8d3f5c99e9b2c91912dd6728e5e
ArjitSingh0907 <singh25arjit@gmail.com>
2026-09-18 20:20:45 +0530

    insertion at end

---
+++ arr_ins0.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[10] = {10, 20, 30, 40, 50};
+    int size = 5, element;
+
+    cout << "Enter element to insert at end: ";
+    cin >> element;
+
+    arr[size] = element;
+    size++;
+
+    cout << "Updated array: ";
+    for(int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}

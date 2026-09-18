dff3bb4e6f455e42f87f6f432d938b1945f0555b
ArjitSingh0907 <singh25arjit@gmail.com>
2026-09-18 20:59:00 +0530

    insert elm in middle of array

---
+++ arr_ins4.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[10] = {11, 22, 33, 44, 55};
+    int size = 5;
+
+    int pos = size / 2;
+    int element = 99;
+
+    for(int i = size; i > pos; i--) {
+        arr[i] = arr[i - 1];
+    }
+
+    arr[pos] = element;
+    size++;
+
+    cout << "Updated array: ";
+
+    for(int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}

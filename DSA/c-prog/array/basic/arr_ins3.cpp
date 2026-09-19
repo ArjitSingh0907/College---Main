23a8fecf70590d040bee62d9518b177f76128556
ArjitSingh0907 <singh25arjit@gmail.com>
2026-09-19 20:22:40 +0530

    array insertion at user specified position

--- DSA/c-prog/array/basic/arr_ins3.cpp
+++ DSA/c-prog/array/basic/arr_ins3.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main(void){
     int arr[20] = {10, 20, 30, 40, 50};
     int size = 5;
     int element, pos;
@@ -24,6 +24,5 @@ int main() {
     for(int i = 0; i < size; i++) {
         cout << arr[i] << " ";
     }
-
     return 0;
 }

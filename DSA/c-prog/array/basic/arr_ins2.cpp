0618d2e80550a3438d46a52cf6a44176cc3978d9
ArjitSingh0907 <singh25arjit@gmail.com>
2026-09-19 20:22:09 +0530

    array insertion at index 2

--- DSA/c-prog/array/basic/arr_ins2.cpp
+++ DSA/c-prog/array/basic/arr_ins2.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main(void){
     int arr[10] = {5, 10, 15, 20, 25};
     int size = 5;
     int pos = 2;
@@ -19,6 +19,5 @@ int main() {
     for(int i = 0; i < size; i++) {
         cout << arr[i] << " ";
     }
-
     return 0;
 }
